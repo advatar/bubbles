d2c1587eff60a84a1b78805b63027354d683ea74
Johan Sellström <johan.sellstrom@ericsson.com>
2013-08-26 19:24:33 +0200

    cleaning up

--- app.js
+++ app.js
@@ -11,14 +11,10 @@ var app = express();
 
 var config = process.env.CONFIG;
 
-if (config)
-{
-  var UAA= config.uaa ? config.uaa : "http://uaa.run.pivotal.io";
-  var API= config.api ? config.api :"http://api.run.pivotal.io";
-
-}
- 
-console.log(config);
+var UAA= (config && config.uaa) ? config.uaa : "http://uaa.run.pivotal.io";
+var API= (config && config.api) ? config.api :"http://api.run.pivotal.io";
+var username = (config && config.username) ? config.username :"johan@sellstrom.me";
+var password = (config && config.password) ? config.password :"kv1stfr1tt";
 
 app.configure(function(){
   app.set('port', process.env.PORT || 2013);
@@ -36,30 +32,10 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-
-app.get('/', function(req, res){
-  res.render('index', { title: 'Bubbles',api:API});
-});
-
-app.get('/tree',checkAuth,function(req,res){
-  res.render('tree', { title: 'Bubbles',data:'flare.json'});
+app.get('/',checkAuth,function(req,res){
+  res.render('index', { title: 'Bubbles'});
 }); 
 
-app.get('/tree/:data',checkAuth,function(req,res){
-  res.render('tree', { title: 'Bubbles',data:req.param('data')});
-}); 
-
-app.get('/bubbles',checkAuth,function(req,res){
-  res.render('bubbles', { title: 'Bubbles',data:'flare.json'});
-}); 
-
-app.get('/bubbles/:data',checkAuth,function(req,res){
-  res.render('bubbles', { title: 'Bubbles',data:req.param('data')});
-}); 
-
-app.get('/treemap',checkAuth,function(req,res){
-  res.render('treemap', { title: 'Treemap' });
-}); 
 
 app.get('/apps',checkAuth,function(req,res){
   root = reset();
@@ -75,7 +51,7 @@ app.get('/apps',checkAuth,function(req,res){
         if (!error)
         {
           organizations = JSON.parse(result.text).resources;
-          console.log(organizations);
+          console.log("organizations",organizations);
           callback(null);
         }
         else
@@ -91,7 +67,7 @@ app.get('/apps',checkAuth,function(req,res){
         if (!error)
         {
           spaces = JSON.parse(result.text).resources;
-          console.log(spaces);
+          console.log("spaces",spaces);
           callback(null);
         }
         else
@@ -107,7 +83,7 @@ app.get('/apps',checkAuth,function(req,res){
         if (!error)
         {
           applications = JSON.parse(result.text).resources;
-          console.log(applications);
+          console.log("applications",applications);
           callback(null);
         }
         else
@@ -150,88 +126,13 @@ app.get('/apps',checkAuth,function(req,res){
         res.json(root[0]); 
       }
       else
-        console.err(err);   
-  });
-}); 
-
-app.get('/users',checkAuth,function(req,res){
-  root = reset();
-  token = req.session.access_token || req.cookies.access_token;
-  async.waterfall(
-    [
-    function(callback){
-      sa
-      .get(API+'/v2/organizations')
-      .set('Authorization', token)
-      .set('Accept', 'application/json')
-      .end(function(error, result){
-        if (!error)
-        {
-          organizations = JSON.parse(result.text).resources;
-          callback(null);
-        }
-        else
-          callback(error);
-      }); 
-    },
-    function(callback){
-      sa
-      .get(API+'/v2/spaces')
-      .set('Authorization', token)
-      .set('Accept', 'application/json')
-      .end(function(error, result){
-        if (!error)
-        {
-          spaces = JSON.parse(result.text).resources;
-          callback(null);
-        }
-        else
-          callback(error);
-      }); 
-    },
-    function(callback){
-      sa
-      .get(API+'/v2/users')
-      .set('Authorization', token)
-      .set('Accept', 'application/json')
-      .end(function(error, result){
-        if (!error)
-        {
-          users = JSON.parse(result.text).resources;
-          console.log(users);
-          callback(null);
-        }
-        else
-          callback(error);
-      }); 
-    }
-    ],
-    function (err) {
-      if (!err){
-        _.each(organizations,function(organization)
-        {
-          insert(root,organization,"-1");
-        });
-
-        _.each(spaces,function(space)
-        {
-          insert(organizations,space,space.entity.organization_guid);
-        });
-
-        _.each(users,function(user)
-        {
-          insert(spaces,user,user.entity.space_guid);
-        });
-        res.json(root[0]); 
-      }
-      else
-        console.err(err);   
+        console.log("err",err);   
   });
 }); 
 
 app.get('/login',function(req,res){
   getToken(
-  {username: config.username, password: config.password},
+  {username: username, password: password},
   function (error,result) {
     if (!error) {
       req.session.access_token = "bearer "+JSON.parse(result.text).access_token;
@@ -247,8 +148,8 @@ app.get('/login',function(req,res){
           req.session.user_id=JSON.parse(result.text).user_id;
           res.cookie('access_token', req.session.access_token, { maxAge: 900000, httpOnly: false});
           res.cookie('user_id', req.session.user_id, { maxAge: 900000, httpOnly: false});
-          console.log(req.session.access_token);
-          console.log(req.session.user_id);
+          console.log("req.session.access_token",req.session.access_token);
+          console.log("req.session.user_id",req.session.user_id);
           res.redirect('/');
         }
         else
@@ -269,7 +170,8 @@ app.get('/logout', function (req, res) {
   delete req.session.access_token;
   delete req.cookies;
   res.redirect('/');
-});    
+});  
+
 function reset()
 {
   return [{guid:-1,name:API,metadata:{guid:"-1"},children:[]}];
@@ -339,7 +241,7 @@ function insert(parents, node, guid)
     if (!node.children) 
       node.children=[];
     node.children.push(quota);
-    console.log(quota);
+    //console.log(quota);
   }
   parent.children.push(node);
 }
